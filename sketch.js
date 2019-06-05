86d79466e9fac3fa4b8e6395b5050ad9b0000188
mkeren22 <51344504+mkeren22@users.noreply.github.com>
2019-06-04 21:28:21 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,18 @@
+var osc; // oscillator to play noise
+
 function setup() {
   createCanvas(1600, 800);
+  osc = new p5.Oscillator();
+  osc.setType('sine');
+  osc.freq(240);
+  osc.amp(0);
+  osc.start();
 }
 
 function draw() {
   stroke(0);      
-  fill(mouseX/6.27, mouseY/3.14, random(0, 255));
+  fill(mouseX, mouseY, random(0, 255));
+  osc.amp(mouseY/1000);
+  osc.freq(mouseX+200);
   ellipse(mouseX, mouseY, 100, 100);
 }
